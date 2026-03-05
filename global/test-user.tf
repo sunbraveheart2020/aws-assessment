5eeb131637d49e3aa69b4fecf51c342c7c311982
happyday <sunfangyong2018@gmail.com>
2026-03-05 15:26:12 +1000

    fix: update code to create Confirmed status user for testing

--- global/test-user.tf
+++ global/test-user.tf
@@ -1,13 +1,6 @@
-resource "aws_cognito_user" "test_user" {
-  user_pool_id = aws_cognito_user_pool.main.id
-  username     = "sunfangyong2018@gmail.com"
-
-  attributes = {
-    email          = "sunfangyong2018@gmail.com"
-    email_verified = "true"
+resource "null_resource" "create_confirmed_user" {
+  provisioner "local-exec" {
+    command = "aws cognito-idp admin-create-user --user-pool-id ${aws_cognito_user_pool.main.id} --username ${var.test_email} --user-attributes Name=email,Value=${var.test_email} Name=email_verified,Value=true --message-action SUPPRESS --region ${var.region} && aws cognito-idp admin-set-user-password --user-pool-id ${aws_cognito_user_pool.main.id} --username ${var.test_email} --password ${var.test_password} --permanent --region ${var.region}"
   }
-
-  temporary_password = "TempPass456!"
-  force_alias_creation = false
-  message_action       = "SUPPRESS"
 }
+
--- global/variables.tf
+++ global/variables.tf
@@ -2,3 +2,18 @@ variable "email" {
   type = string
   default = "sunfangyong2018@gmail.com"
 }
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "test_email" {
+  type = string
+  default = "sunfangyong2018@gmail.com"
+}
+
+variable "test_password" {
+  type = string
+  default = "TempPass456!"
+}
